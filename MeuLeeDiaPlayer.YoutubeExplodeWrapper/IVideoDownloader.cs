ffd3914453f075763f38d1eaae943ff6e729f32d
MeuLee <mathieularivee00@gmail.com>
2020-11-15 20:44:32 -0500

    Changed project ver to .NET 5, built wrapper around YoutubeExplode lib, made Console project to test wrapper

---
+++ MeuLeeDiaPlayer.YoutubeExplodeWrapper/IVideoDownloader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MeuLeeDiaPlayer.YoutubeExplodeWrapper
+{
+    public interface IVideoDownloader
+    {
+        Task DownloadVideo(string filePath, string videoIdOrUrl, IProgress<double>? progress, CancellationToken token = default);
+        Task DownloadPlaylist(string folder, string playlistIdOrUrl, IProgress<double> progress, CancellationToken token = default);
+    }
+}
---
+++ MeuLeeDiaPlayer.YoutubeExplodeWrapper/YoutubeVideoDownloader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Playlists;
+using YoutubeExplode.Videos;
+using YoutubeExplode.Videos.Streams;
+
+namespace MeuLeeDiaPlayer.YoutubeExplodeWrapper
+{
+    public class YoutubeVideoDownloader : IVideoDownloader
+    {
+        private readonly YoutubeClient _ytClient;
+        private readonly StreamClient _streamClient;
+        private readonly PlaylistClient _playlistClient;
+        private readonly VideoClient _videoClient;
+
+        public YoutubeVideoDownloader()
+        {
+            _ytClient = new YoutubeClient();
+            _streamClient = _ytClient.Videos.Streams;
+            _playlistClient = _ytClient.Playlists;
+            _videoClient = _ytClient.Videos;
+        }
+
+        public async Task DownloadVideo(string folder, string videoIdOrUrl, IProgress<double>? progress, CancellationToken token = default)
+        {
+            var videoId = new VideoId(videoIdOrUrl);
+
+            var videoInfo = await _videoClient.GetAsync(videoId);
+            var streamManifest = await _streamClient.GetManifestAsync(videoId).ConfigureAwait(false);
+            var streamInfo = streamManifest.GetAudioOnly().WithHighestBitrate();
+
+            string filePath = $"{folder}\\{videoInfo.Title}.{streamInfo.Container.Name}";
+            await _streamClient.DownloadAsync(streamInfo, filePath, progress, token).ConfigureAwait(false);
+        }
+
+        public async Task DownloadPlaylist(string folder, string playlistIdOrUrl, IProgress<double>? progress, CancellationToken token = default)
+        {
+            var playlistId = new PlaylistId(playlistIdOrUrl);
+            var videos = (await _playlistClient.GetVideosAsync(playlistId)).ToList();
+
+            var multiProgress = new MultiProgress(progress, videos.Count);
+            foreach (var video in videos)
+            {
+                multiProgress.Add(video.Id.Value);
+                var currentProgress = new Progress<double>(p => multiProgress.Update(video.Id.Value, p));
+                await DownloadVideo(folder, video.Id, currentProgress, token);
+            }
+        }
+    }
+}
